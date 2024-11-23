0e4a929bb8b0b78689cd384bfcf2262dfbae6af7
sasha6839 <saskohrapan@gmail.com>
2024-11-23 12:00:48 +0200

    my_bot

--- help-1/index.md
+++ help-1/index.md
@@ -1,12 +1,11 @@
 ## Розробка чат-ботів в Python
 
-[HackIt](it/hackit.md)
+[HackIt](hackit.jpeg)
 
 ---
 
 ### Зміст 
 
 1. [Знайомство з TelegramBotApi](lesson01/help.md)
-2. [Обробка повідомлень, клавіатура](lesson02/help.md)
-3.  ...
-
+2. ...
+gdegdgrxfb
--- main.py
+++ main.py
@@ -1,12 +1,61 @@
 import telebot
+from telebot import types
 
 token = '7724717353:AAH1itkQBgSDAYcMHS402kXqqE7LLcryJSE'
-
 bot = telebot.TeleBot(token)
+# створюємо об'єкт клавіатури.
+# my_buttons = types.ReplyKeyboardMarkup()
+# створюємо окрему кнопку.
+# btn1 = types.KeyboardButton('Кнопка 1')
+# btn2 = types.KeyboardButton('Кнопка 2')
+# додаємо кнопки до клавіатури.
+# my_buttons.add(btn1, btn2)
 
-@bot.message_handler(content_types=['text'])
-def botmessage(message):
-    mes = message.text + ' - Це написав класний хлопець!'
+# --- BOT MESSAGE ---------------------------------------------------
+
+
+
+@bot.message_handler(commands=['start', 'stop', 'b', ''])
+def bot_commands(message):
+    mes = ''
+
+    if message.text == '/start':
+        mes = 'почати'
+    elif message.text == '/stop':
+        mes = 'зупинити'
+    elif message.text == '/b':
+        bot_buttons(message)
+        return True
+
     bot.send_message(message.chat.id, mes)
 
-bot.polling()
+@bot.message_handler(content_types=['text'])
+def bot_message(message):
+    mes = message.text + ' - !'
+    if message.text == 'Саня підор':
+        mes = 'Іди нахуй'
+    elif message.text == 'Тімур підор':
+        mes = 'Звісно'
+
+    bot.send_message(message.chat.id, mes)
+
+def bot_buttons(message):
+    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+
+    btn1 = types.KeyboardButton(text='/start')
+    btn2 = types.KeyboardButton(text='/stop')
+    btn3 = types.KeyboardButton(text='/b')
+
+    keyboard.add(btn1, btn2, btn3)
+
+    mes = bot.send_message(message.chat.id, message.text, reply_markup=keyboard)
+    bot.register_next_step_handler(msg, button_if)
+
+def button_if(message):
+    if message.text == '':
+        bot.send_message(message.chat.id, 'Кнопка')
+    if message.text == '':
+        bot.send_message(message.chat.id, 'Кнопка')
+
+if __name__ == '__main__':
+    bot.polling()
